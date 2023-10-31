3c4b55741690cebc8d1c101554cfeecf3df858ec
Sergio <114320133+sergioing1000@users.noreply.github.com>
2023-10-30 22:00:42 -0500

    incluyendo server.js para conectar mongodb

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { DB_HOST, HOST } = process.env;
+const PORT = process.env.PORT || 4005;
+
+const app = require('./app');
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(PORT, HOST, () => {
+      console.log('Server is listening on', HOST + ':' + PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+// Define tus rutas aquí, después de la definición de app y antes de la llamada a app.listen
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
