944309a781d843e4bbf634a3480fe649e8ccdf15
EmmanuelSGiraldo <esemmanuel2022@gmail.com>
2023-11-05 23:45:08 -0500

    fixerror products

--- models/product.js
+++ models/product.js
@@ -1,21 +1,23 @@
-// const { string } = require("joi");
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const productSchema = Schema(
   {
     categories: { type: Array },
     weight: { type: Number },
     title: {
-      type: String
-        },
+      type: {
+        ua: { type: String },
+        en: {type: String}
+      },
+    },
     calories: { type: Number },
     groupBloodNotAllowed: {
       type: Array,
     },
   },
-  { versionKey: false, timestamps: true }
+  { versionKey: false, timestamps: true },
 );
 
-const Product = model("product", productSchema);
+const Product = model('product', productSchema);
 
 module.exports = { Product };
